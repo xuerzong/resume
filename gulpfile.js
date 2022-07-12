4b56ab7648ba5f4c2f0afc0009d77e77ceb49962
xucong <xuerzong@gmail.com>
2022-07-12 20:13:24 +0800

    fix: dev mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,54 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss')
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const browserSync = require('browser-sync').create();
+const buildHtml = require('./scripts/build-html')
 
 const htmlmin = require('gulp-htmlmin')
+const isDev = process.env.NODE_ENV === 'development'
 
-gulp.task('css', () => 
-  gulp.src('./styles/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(postcss([
-    autoprefixer(),
-    cssnano()
-  ]))
-  .pipe(gulp.dest('dist/styles'))
-)
+const devPort = parseInt(process.env.PORT || 3000, 10)
+const outputPath = process.env.OUTPUT_PATH || './dist'
 
-gulp.task('html-minify', () =>
-  gulp.src('./dist/index.html')
+gulp.task('html', () => {
+  buildHtml()
+  return gulp.src('./dist/index.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist'))
-)
+    .pipe(gulp.dest(outputPath))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('css', () => {
+  const postcssPlugins = [autoprefixer()]
+  if(!isDev) {
+    postcssPlugins.push(cssnano())
+  }
+  return gulp.src('./styles/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss(postcssPlugins))
+  .pipe(gulp.dest(`${outputPath}/styles`))
+  .pipe(browserSync.stream())
+})
+
+gulp.task('watch', () => {
+  browserSync.init({
+    server: `${outputPath}`,
+    port: devPort,
+    ui: false
+  })
+  gulp.watch([
+    './index.html',
+    './constants/**/*.js',
+    './core/**/*.js',
+    './scripts/build-html.js',
+    './templates/**/*.html',
+    './utils/**/*.js'
+  ], gulp.series('html'))
+  gulp.watch('./styles/**/*.scss', gulp.series('css'))
+})
+
+gulp.task('build', gulp.series('html', 'css'))
+
+gulp.task('default', gulp.series('html', 'css', 'watch'))
--- scripts/build-html.js
+++ scripts/build-html.js
@@ -4,19 +4,25 @@ const { ROOT_DIR } = require('../constants/path')
 const Base = require('../core/base')
 const { loadConfigYaml, loadIndexTemplate } = require('../utils/file')
 
-let indexHtml = loadIndexTemplate()
-const { base: _base } = loadConfigYaml();
+const outputPath = process.env.OUTPUT_PATH || './dist'
 
-const base = new Base(_base)
-
-const baseRender = base.render()
-
-indexHtml = indexHtml.replace('<body>', `<body>${baseRender}`)
-
-const outputDir = path.resolve(ROOT_DIR, './dist')
-
-if(!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir)
+const buildHtml = () => {
+  let indexHtml = loadIndexTemplate()
+  const { base: _base } = loadConfigYaml();
+  
+  const base = new Base(_base)
+  
+  const baseRender = base.render()
+  
+  indexHtml = indexHtml.replace('<body>', `<body>${baseRender}`)
+  
+  const outputDir = path.resolve(ROOT_DIR, outputPath)
+  
+  if(!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir)
+  }
+  
+  fs.writeFileSync(path.resolve(outputDir, 'index.html'), indexHtml, 'utf-8')
 }
 
-fs.writeFileSync(path.resolve(outputDir, 'index.html'), indexHtml, 'utf-8')
+module.exports = buildHtml
