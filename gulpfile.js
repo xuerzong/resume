620480926a37486c31478edeb6c450a6fb999727
xuerzong <xuerzong@gmail.com>
2023-02-23 09:38:46 +0800

    feature: v1.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
+const path = require('path')
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const { OUTPUT_PATH } = require('./constants/path')
+
+gulp.task('images', () => {
+  return gulp.src('./assets/images/*')
+      .pipe(gulp.dest(path.resolve(OUTPUT_PATH, 'images')))
+});
 
 gulp.task('css', () => {
   const postcssPlugins = [autoprefixer(), cssnano()]
